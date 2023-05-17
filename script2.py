8699375712a87648719a7115330b60ab76d4c070
ashuakhil <133909650+ashuakhil@users.noreply.github.com>
2023-05-17 16:30:16 -0400

    Modified both files in the repo clone
    - Deleted a print() statement from script1.py
    - Added one print() statement to script2.py

--- script2.py
+++ script2.py
@@ -1,4 +1,4 @@
 # Print lines from Zen of Python
 print("Explicit is better than implicit.")
 print("Flat is better than nested.")
-
+print("Namespaces are one honking great idea -- let's do more of those!")
